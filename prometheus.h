8ad752951351828018ea282c3cfb1216a09ff416
Alex Chistyakov <alexclear@gmail.com>
2016-11-15 16:46:54 +0300

    Add histogram functions

--- prometheus.h
+++ prometheus.h
@@ -2,4 +2,7 @@
 #define PROMETHEUS_H
 int new_counter_vec(char* name, char* help, char** labels, int nlabels);
 int increment_counter(char* name, char** labels, int nlabels);
+int new_histogram_vec(char* name, char* help, char** labels, int nlabels, double* bucket_margins, int nbuckets);
+int observe_histogram(char* name, char** labels, int nlabels, double value);
+int print_metrics();
 #endif
